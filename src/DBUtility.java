248961f3f2d8995bd4022367a03e14545a651688
dylan <dylanelliott27@hotmail.com>
2020-10-19 21:13:50 -0400

    seperate connection, column parsing, and row parsing into seperate methods

--- src/DBUtility.java
+++ src/DBUtility.java
@@ -1,33 +1,49 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+import java.sql.*;
+import java.util.Date;
 import java.util.Properties;
 
 public class DBUtility {
-    private Connection connection;
-    private PreparedStatement sqlStatement;
 
-    public void fetchResourceHistory(){
+    public static void fetchResourceHistory(){
+        Connection connection = connectToLocalDB();
+        PreparedStatement sqlStatement = null;
         try{
-        Properties connSettings = new Properties();
-        connSettings.put("user", "root");
-        connection = DriverManager.getConnection("JDBC:mysql://localhost:3306/resourcemonitor", connSettings);
         String statement = "SELECT * from resourcehistory;";
 
         sqlStatement = connection.prepareStatement(statement);
 
         ResultSet results = sqlStatement.executeQuery();
 
-            while (results.next()) {
-                // Read values using column name
-                String logID = results.getString("logid");
-                int cpuUsage = results.getInt("cpuusage");
-                int hddSpace = results.getInt("hddspace");
-                int ramUsage = results.getInt("ramusage");
-                String operatingSystem = results.getString("operatingsystem");
+        System.out.println(parseColumnNames(results));
+        parseAndPrintRowData(results);
 
-                System.out.printf(logID + ' ' + cpuUsage + ' ' + hddSpace + ' ' + ramUsage + ' ' + operatingSystem);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private static Connection connectToLocalDB(){
+        Connection connection = null;
+        try{
+            Properties connSettings = new Properties();
+            connSettings.put("user", "root");
+            connection = DriverManager.getConnection("JDBC:mysql://localhost:3306/resourcemonitor", connSettings);
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    private static String parseColumnNames(ResultSet queryResult){
+        String columnNames = "";
+        try{
+            ResultSetMetaData metadata = queryResult.getMetaData();
+
+            for( int i = 1; i <= metadata.getColumnCount(); i++){
+                columnNames += metadata.getColumnName(i) + " ";  // Doing it this way just so it shows best in console
             }
 
         }
@@ -35,5 +51,24 @@ public class DBUtility {
             e.printStackTrace();
         }
 
+        return columnNames;
+    }
+
+    private static void parseAndPrintRowData(ResultSet queryResult){
+        // May change to return something in future so can be accessed in arrayList or something
+        try{
+            while (queryResult.next()) {
+                String logID = queryResult.getString("logid");
+                Date logDate = queryResult.getDate("logdate");
+                int cpuUsage = queryResult.getInt("cpuusage");
+                int hddSpace = queryResult.getInt("hddspace");
+                int ramUsage = queryResult.getInt("ramusage");
+                String operatingSystem = queryResult.getString("operatingsystem");
+                System.out.println(logID + ' ' + logDate + ' ' + cpuUsage + ' ' + hddSpace + ' ' + ramUsage + ' ' + operatingSystem);
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
