ff42d2822720df2b8aa99b675424fc69f11c58cd
dylan <dylanelliott27@hotmail.com>
2020-10-20 20:16:27 -0400

    cleanup whitespace/format code

--- src/DBUtility.java
+++ src/DBUtility.java
@@ -3,60 +3,52 @@ import java.util.Date;
 import java.util.Properties;
 
 public class DBUtility {
-
-    public static void fetchResourceHistory(){
+    // TODO --- JAVADOC COMMENTS
+    public static void fetchResourceHistory() {
         Connection connection = connectToLocalDB();
         PreparedStatement sqlStatement = null;
-        try{
-        String statement = "SELECT * from resourcehistory;";
-
-        sqlStatement = connection.prepareStatement(statement);
-
-        ResultSet results = sqlStatement.executeQuery();
-
-        System.out.println(parseColumnNames(results));
-        parseAndPrintRowData(results);
-
-        }
-        catch(Exception e){
+        try {
+            String statement = "SELECT * from resourcehistory;";
+            sqlStatement = connection.prepareStatement(statement);
+            ResultSet results = sqlStatement.executeQuery();
+            System.out.println(parseColumnNames(results));
+            parseAndPrintRowData(results);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-
     }
 
-    private static Connection connectToLocalDB(){
+    private static Connection connectToLocalDB() {
         Connection connection = null;
-        try{
+        try {
             Properties connSettings = new Properties();
             connSettings.put("user", "root");
             connection = DriverManager.getConnection("JDBC:mysql://localhost:3306/resourcemonitor", connSettings);
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
         return connection;
     }
 
-    private static String parseColumnNames(ResultSet queryResult){
+    private static String parseColumnNames(ResultSet queryResult) {
         String columnNames = "";
-        try{
+        try {
             ResultSetMetaData metadata = queryResult.getMetaData();
 
-            for( int i = 1; i <= metadata.getColumnCount(); i++){
+            for (int i = 1; i <= metadata.getColumnCount(); i++) {
                 columnNames += metadata.getColumnName(i) + " ";  // Doing it this way just so it shows best in console
             }
 
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         return columnNames;
     }
 
-    private static void parseAndPrintRowData(ResultSet queryResult){
-        // May change to return something in future so can be accessed in arrayList or something
-        try{
+    private static void parseAndPrintRowData(ResultSet queryResult) {
+        // Will change to return something in future so can be accessed in arrayList or something
+        try {
             while (queryResult.next()) {
                 String logID = queryResult.getString("logid");
                 Date logDate = queryResult.getDate("logdate");
@@ -66,8 +58,7 @@ public class DBUtility {
                 String operatingSystem = queryResult.getString("operatingsystem");
                 System.out.println(logID + ' ' + logDate + ' ' + cpuUsage + ' ' + hddSpace + ' ' + ramUsage + ' ' + operatingSystem);
             }
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/ResourceMonitorModel.java
+++ src/ResourceMonitorModel.java
@@ -1,7 +1,5 @@
 public class ResourceMonitorModel {
     public static void main(String[] args) {
-        DBUtility dbtest = new DBUtility();
-
-        dbtest.fetchResourceHistory();
+        DBUtility.fetchResourceHistory();
     }
 }
